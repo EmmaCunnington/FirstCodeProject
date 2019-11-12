3bdab29b648cdf2781b75662d7483adf0dbace3c
EmmaCunnington <57588491+EmmaCunnington@users.noreply.github.com>
2019-11-12 13:42:38 +1000

    trying to fix syntax rock paper scissors

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -17,21 +17,22 @@ message = {
 }
 
 def decide_winner(user_choice, computer_choice):
-  print 'You selected: %s' % user_choice
-  print 'Computer selected: %s' % computer_choice
+  print ('You selected: %s') % (user_choice) #why doesn't it like this??
+  # unsupported operand type(s) for %: 'NoneType' and 'str'
+  print ('Computer selected: %s') % (computer_choice)
   if user_choice == computer_choice:
-    print message['tie']
+    print (message['tie'])
   elif user_choice == options[0] and computer_choice == options[2]:
-    print message['won']
+    print (message['won'])
   elif user_choice == options[1] and computer_choice == options[0]:
-    print message['won']
+    print (message['won'])
   elif user_choice == options[2] and computer_choice == options[1]:
-    print message['won']
+    print (message['won'])
   else:
-    print message['lost']
+    print (message['lost'])
     
 def play_RPS():
-  user_choice = raw_input('Enter Rock, Paper, or Scissors: ')
+  user_choice = input('Enter Rock, Paper, or Scissors: ')
   user_choice = user_choice.upper()
   computer_choice = options[randint(0, 2)]
   decide_winner(user_choice, computer_choice)
