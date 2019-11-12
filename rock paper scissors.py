66370dd4a98c51beb823219c2d3b6bf4b5ff756d
EmmaCunnington <57588491+EmmaCunnington@users.noreply.github.com>
2019-11-12 13:35:38 +1000

    update RPS

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -11,14 +11,14 @@ from random import randint
 options = ['ROCK', 'PAPER', 'SCISSORS']
 
 message = {
-  'tie': 'Yawn it/s a tie!',
+  'tie': "Yawn it's a tie!",
   'won': 'Yay you won!',
   'lost': 'Aww you lost!'
 }
 
 def decide_winner(user_choice, computer_choice):
-  print 'You selected: %s' % (user_choice)
-  print 'Computer selected: %s' % (computer_choice)
+  print 'You selected: %s' % user_choice
+  print 'Computer selected: %s' % computer_choice
   if user_choice == computer_choice:
     print message['tie']
   elif user_choice == options[0] and computer_choice == options[2]:
@@ -31,8 +31,10 @@ def decide_winner(user_choice, computer_choice):
     print message['lost']
     
 def play_RPS():
-  user_choice = (raw_input('Enter Rock, Paper, or Scissors: ')).upper
+  user_choice = raw_input('Enter Rock, Paper, or Scissors: ')
+  user_choice = user_choice.upper()
   computer_choice = options[randint(0, 2)]
   decide_winner(user_choice, computer_choice)
   
-play_RPS()
+while(1):
+  play_RPS()
